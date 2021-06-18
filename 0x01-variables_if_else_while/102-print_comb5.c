6c9782ed4454ad34480eb32d274b29f62b79ec8d
Selam Ayehubirhan <kabodshekinah@gmail.com>
2021-06-18 05:34:31 -0700

    if else last advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - create two pair of numbers that don't repeat
+* Return: 0
+*/
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 99; j++)
+{
+for (i = j; i <= 99; i++)
+{
+if (i != j)
+{
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
+putchar(' ');
+putchar(i / 10 + 48);
+putchar(i % 10 + 48);
+
+if (j * 100 + i != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
