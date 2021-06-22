62c6c25ff8d979b9f5aa1465b0700c5ff5a6c3bb
Selam Ayehubirhan <kabodshekinah@gmail.com>
2021-06-22 03:45:31 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,15 +10,5 @@
 
 int _islower(int c)
 {
-	int i;
-
-	if (c >= 'A' && c <= 'Z')
-	{
-		i = 0;
-	}
-	else if (c >= 'a' && c <= 'z')
-	{
-		i = 1;
-	}
-	return (i);
+return (c >= 97 && c <= 122);
 }
