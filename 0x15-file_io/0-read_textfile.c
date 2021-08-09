ed9bd168e65fd7117cfa1db9507747ffc58ddb26
Selam Ayehubirhan <kabodshekinah@gmail.com>
2021-08-09 02:32:14 -0700

    try

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,33 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - function to read a text file and print to stdout
+ * @filename: the name of the file
+ * @letters: the number of letters to be printed
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
+ * Return: actual number of letters that was printed
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+ssize_t fdo, fdr, fdw;
+char *buf;
 
-	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
-	return (w);
+if (filename == NULL)
+return (0);
+fdo = open(filename, O_RDONLY);
+if (fdo == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+fdr = read(fdo, buf, letters);
+if (fdr == -1)
+return (0);
+fdw = write(STDOUT_FILENO, buf, fdr);
+if (fdw == -1)
+return (0);
+free(buf);
+close(fdo);
+return (fdw);
 }
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,7 +1,1 @@
-#3 0x15. C - File I/O
-1. Tread lightly, she is near 
-2. Under the snow 
-3. Speak gently, she can hear 
-4. a program that copies the content of a file to another file. 
-5. elf 
-
+0x15. C - File I/O
