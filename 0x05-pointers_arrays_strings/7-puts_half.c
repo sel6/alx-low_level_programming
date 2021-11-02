64bab5d76803f8715856c61815d0d6f7c84efa25
Selam <84750917+sel6@users.noreply.github.com>
2021-11-02 16:04:06 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,32 +5,22 @@
  * @s: string s
  * Return: length of string
  */
-int _strlen(char *s)
+int _strlen(char *str)
 {
-
-	int length = 0;
-
-	while (*s)
-	{
-		s++;
-		length++;
-	}
-	return (length);
+   int i=0;
+    while(str[i])
+    i++;
+    return i;
 }
-
-/**
- * puts_half - prints the second half of the string
- * @str: string
- */
-
 void puts_half(char *str)
 {
-	int length, i, half;
-
-	length = _strlen(str);
-	half = (length % 2 == 0) ? length / 2 : (length - 1) / 2 + 1;
-
-	for (i = half; i < length; i++)
-		_putchar(*(str + i));
-	_putchar('\n');
-}
+    int len = _strlen(str);
+    int n;
+    int i;
+    n = (len % 2 == 0) ? len/2 : (len-1)/2 + 1;
+    for(i =n; i<len; i++)
+    {
+        _putchar(str[i]);
+    }
+       
+    }
